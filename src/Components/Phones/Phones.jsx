36ccc53a32807d910530e1e14337ef7e439e3e84
emdad118661 <emdad118661@gmail.com>
2024-04-16 10:22:50 +0600

    loading spinner

--- src/Components/Phones/Phones.jsx
+++ src/Components/Phones/Phones.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
+import { Circles } from 'react-loader-spinner'
 
 const Phones = () => {
     const [phones, setPhones] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -23,10 +24,20 @@ const Phones = () => {
         })
         console.log(phonesWithFakeData);
         setPhones(phonesWithFakeData);
+        setLoading(false);
        })
     }, [])
     return (
         <div>
+            {loading && <Circles
+  height="80"
+  width="80"
+  color="#4fa94d"
+  ariaLabel="circles-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  visible={true}
+  />}
             <h2 className="text-5xl mt-8">Phones: {phones.length}</h2>
             <BarChart width={1200} height={400} data={phones}>
           <Bar dataKey="price" fill="#8884d8" />
